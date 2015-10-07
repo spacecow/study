06ac4f46d588ecef4550d91a442ad628f1a71766
Johan Sveholm <jsveholm@gmail.com>
2015-10-07 13:53:00 +0200

    quiz only sentences with translations

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,7 +4,7 @@ class QuizzesController < ApplicationController
   end
 
   def new
-    quiz = Quiz.factory questionables:Sentence.all
+    quiz = Quiz.factory questionables:Sentence.where('english != ?','')
     redirect_to new_answer_url(quiz_id:quiz.id, question_id:quiz.questions.first.id)
   end
 
